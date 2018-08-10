294c2f47a86319bb51d1e24ea46e636b8ed1cbc6
Brad McCollum <btmcco8@gmail.com>
2018-08-10 17:07:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,6 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
-  # attendee_array = []
-  # array.each{|attendee_name| attendee_array << badge_maker(attendee_name)}
-  # attendee_array
+def batch_badge_creator(array) #takes in an array of names, iterates over each and creates a new array with their badges
   array.map{|attendee_name| badge_maker(attendee_name)}
 end
