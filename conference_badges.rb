e89d3ac339eb936f8b359c7ed6f1e34197691e6e
Brad McCollum <btmcco8@gmail.com>
2018-08-10 17:13:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,6 +8,6 @@ end
 
 def assign_rooms(array)
   room_assignments = []
-  array.each_index{|room_number| puts "Hello, #{array}! You'll be assigned to room #{room_number}" }
+  array.each_index{|room_number| puts "Hello, #{index}! You'll be assigned to room #{room_number}" }
   room_assignments
 end
