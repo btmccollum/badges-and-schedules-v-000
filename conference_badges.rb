ff517e0c567e359ae16834b7601153ea6a29988f
Brad McCollum <btmcco8@gmail.com>
2018-08-10 17:05:36 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,8 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  attendee_array = []
-  array.each{|attendee_name| attendee_array << badge_maker(attendee_name)}
-  attendee_array
+  # attendee_array = []
+  # array.each{|attendee_name| attendee_array << badge_maker(attendee_name)}
+  # attendee_array
+  array.map{|attendee_name| badge_maker(attendee_name)}
 end
