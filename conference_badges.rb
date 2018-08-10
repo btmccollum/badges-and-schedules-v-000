b3b9bd3a712f7ba9f3608bba00f12fc9fa717e0a
Brad McCollum <btmcco8@gmail.com>
2018-08-10 17:10:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,9 @@ end
 def batch_badge_creator(array) #takes in an array of names, iterates over each and creates a new array with their badges
   array.map{|attendee_name| badge_maker(attendee_name)}
 end
+
+def assign_rooms(array)
+  room_assignments = []
+  array.each_with_index{|room_number| puts "Hello, #{attendee_name}! You'll be assigned to room #{room_number}" }
+  room_assignments
+end
